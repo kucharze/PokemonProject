4b4c3599c96c633c8abbab95a263e9214346f54c
Zachary Kuchar <zacharyk323@live.com>
2023-07-26 14:26:28 -0400

    Initial setup practice with API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [pokemon, setPokemon] = useState(null);
+  const [moves, setMoves] = useState(null);
+
+  const getPokemon = async () => {
+    try {
+      let item = await fetch("https://pokeapi.co/api/v2/pokemon/charmander");
+      let data = await item.json();
+      console.log(data);
+      setPokemon(data);
+      //getMove();
+    } catch (err) {
+      console.log("We did not find a valid pokemon");
+    }
+  };
+
+  const getMove = async () => {
+    // let item = await fetch(`https://pokeapi.co/api/v2/move/851/`);
+    let moveList = [];
+    for (let i = 0; i < 4; i++) {
+      let item = await fetch(
+        `${
+          pokemon.moves[Math.floor(Math.random() * (100 - 1 + 1) + 1)].move.url
+        }`
+      );
+      let data = await item.json();
+      console.log("move", data);
+      moveList.push(data);
+    }
+    await setMoves(moveList);
+  };
+
+  useEffect(() => {
+    getPokemon();
+    // getMove();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      test
+      {pokemon && (
+        <div>
+          <h1>{pokemon.name}</h1>
+          <h1>Experience: {pokemon.base_experience}</h1>
+          <h1>Pics</h1>
+          <img src={pokemon.sprites.front_default} alt="" />
+          <img src={pokemon.sprites.back_default} alt="" />
+          <h2>Access a move: {pokemon.moves[5].move.name}</h2>
+          {}
+
+          {moves ? (
+            moves.map((item) => {
+              console.log(item.name);
+              return <h3>Move ID: {item.name}</h3>;
+            })
+          ) : (
+            <h3>Loading</h3>
+          )}
+
+          <button onClick={getMove}>Load moves</button>
+        </div>
+      )}
     </div>
   );
 }
